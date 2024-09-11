f5cf720fc9540d7e16d9ac91cf39be480a50422e
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-09-11 20:23:51 +0700

    sample

---
+++ Task_1_1_1/src/main/java/ru/nsu/laptev/Main.java
@@ -0,0 +1,13 @@
+package ru.nsu.laptev;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static int sum(int a, int b)
+    {
+        return a + b;
+    }
+    public static void main(String[] args)
+    {
+    }
+}
