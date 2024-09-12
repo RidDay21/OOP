382294a17a0b0fe545ac535c374bd49a2aa7a2d3
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-09-12 10:26:40 +0700

    write a code

---
+++ Task_1_1_1/src/main/java/ru/nsu/laptev/HeapSort.java
@@ -0,0 +1,71 @@
+package ru.nsu.laptev;
+
+import java.io.*;
+
+public class HeapSort {
+
+    public static void addAncestor(int index, int data, int[] BinTree) {
+        int i = index;
+        int indPred = (i - 1) / 2;
+
+        BinTree[i] = data;//вносим новый элемент в наше дерево
+
+        while (i > 0 && BinTree[i] < BinTree[indPred]) {//тут мы просто просеиваем вверх наше дерево
+            int tmp = BinTree[i];
+            BinTree[i] = BinTree[indPred];
+            BinTree[indPred] = tmp;
+
+            i = indPred;
+            indPred = (i - 1) / 2;
+        }
+    }
+
+    // Метод для преобразования в кучу
+    public static void heapify(int index, int[] list, int size) {
+        while (index < size / 2) {
+            int indLeftAncestor = 2 * index + 1;
+            int indRightAncestor = 2 * index + 2;
+
+            int indBossAncestor = (indRightAncestor < size && list[indLeftAncestor] > list[indRightAncestor]) ? indRightAncestor : indLeftAncestor;
+
+            if (list[indBossAncestor] < list[index])
+            {
+                int tmp = list[indBossAncestor];
+                list[indBossAncestor] = list[index];
+                list[index] = tmp;
+                index = indBossAncestor;
+            }
+            else
+                break;
+        }
+    }
+
+    public static void heapsort(int [] array, int length)
+    {
+        int [] BinTree = new int [length];
+        for (int i = 0; i < length;i++)
+        {
+            int anc = array[i];
+            addAncestor(i,anc,BinTree);
+        }
+
+        for (int i = 0; i < length;i++)
+        {
+            heapify(i,BinTree,length);
+        }
+
+        int[] sortedArray = new int[length];
+
+        for (int i =0; i < length;i++)
+        {
+            sortedArray[i] = BinTree[0];
+            BinTree[0] = BinTree[length - i - 1];
+            heapify(0,BinTree,length - i - 1);
+        }
+
+        for (int i = 0;i < length;i++)
+        {
+            System.out.print(sortedArray[i] + " ");
+        }
+    }
+}
--- Task_1_1_1/src/main/java/ru/nsu/laptev/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/laptev/Main.java
@@ -3,11 +3,10 @@ package ru.nsu.laptev;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static int sum(int a, int b)
-    {
-        return a + b;
-    }
     public static void main(String[] args)
     {
+        int [] array = {34,5,23,543544,1,5,78,3};
+        HeapSort.heapsort(array, array.length);
+
     }
 }
