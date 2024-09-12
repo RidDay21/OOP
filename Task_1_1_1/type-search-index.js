d63399c9503d4000a349ea8793a32d1f19d41520
RidDay21 <RidDay21@users.noreply.github.com>
2024-09-12 06:46:06 +0000

    Deploying to gh-pages from @ RidDay21/OOP@84edba1a51e170ab6c345f5488ddc45621d2a258 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.laptev","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.laptev","l":"HeapSort"},{"p":"ru.nsu.laptev","l":"Main"}]
