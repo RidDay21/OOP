0084d6f6e1139cb9a8ce7957d6e4020672f41149
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-09-12 19:50:39 +0700

    a

--- Task_1_1_1/src/main/java/ru/nsu/laptev/HeapSort.java
+++ Task_1_1_1/src/main/java/ru/nsu/laptev/HeapSort.java
@@ -3,17 +3,20 @@ package ru.nsu.laptev;
 import java.io.*;
 
 /**
- *
+ * Class for realization of HeapSort
  */
 public class HeapSort {
 
+    /**
+     * Метод для добавления предка в бинарное дерево
+     */
     public static void addAncestor(int index, int data, int[] bintree) {
         int i = index;
         int indPred = (i - 1) / 2;
 
-        bintree[i] = data; //вносим новый элемент в наше дерево
+        bintree[i] = data;
 
-        while (i > 0 && bintree[i] < bintree[indPred]) { //тут мы просто просеиваем вверх наше дерево
+        while (i > 0 && bintree[i] < bintree[indPred]) {
             int tmp = bintree[i];
             bintree[i] = bintree[indPred];
             bintree[indPred] = tmp;
@@ -22,7 +25,9 @@ public class HeapSort {
         }
     }
 
-    // Метод для преобразования в кучу
+    /**
+     * Метод "просеивания"
+     */
     public static void heapify(int index, int[] list, int size) {
         while (index < size / 2) {
             int indLeftAncestor = 2 * index + 1;
@@ -31,14 +36,20 @@ public class HeapSort {
             int indBossAncestor = (indRightAncestor < size && list[indLeftAncestor] > list[indRightAncestor]) ? indRightAncestor : indLeftAncestor;
 
             if (list[indBossAncestor] < list[index]) {
+                // Процесс просеивания вниз для поддержания свойства кучи
                 int tmp = list[indBossAncestor];
                 list[indBossAncestor] = list[index];
                 list[index] = tmp;
                 index = indBossAncestor;
-            } else break;
+            } else {
+                break;
+            }
         }
     }
 
+    /**
+     * Метод для пирамидальной сортировки
+     */
     public static void heapsort(int[] array, int length) {
         int[] BinTree = new int[length];
         for (int i = 0; i < length; i++) {
