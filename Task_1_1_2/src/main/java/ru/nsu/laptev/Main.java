a6f049d46f177d73c405eecf352a2628b99c6a6d
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-09-17 21:50:43 +0700

    lesgo

--- Task_1_1_2/src/main/java/ru/nsu/laptev/Main.java
+++ Task_1_1_2/src/main/java/ru/nsu/laptev/Main.java
@@ -4,10 +4,6 @@ package ru.nsu.laptev;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    }
-
-    public static int multiply(int a, int b) {
-        return a * b;
+        System.out.println("Bababoy");
     }
 }
-
