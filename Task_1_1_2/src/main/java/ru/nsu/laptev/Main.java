4a7c17762117f79e04561a4cfceb24550b0f9627
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-09-17 20:16:10 +0700

    lesgo

--- Task_1_1_2/src/main/java/ru/nsu/laptev/Main.java
+++ Task_1_1_2/src/main/java/ru/nsu/laptev/Main.java
@@ -5,8 +5,9 @@ package ru.nsu.laptev;
 public class Main {
     public static void main(String[] args) {
     }
-    public static int multiply(int a,int b)
-    {
-        return a*b;
+
+    public static int multiply(int a, int b) {
+        return a * b;
     }
+}
 
