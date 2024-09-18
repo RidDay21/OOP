155ccda496091b19d48a1f2393c79c972058bbd1
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-09-18 11:38:45 +0700

    lesgo

--- Task_1_1_2/src/main/java/ru/nsu/laptev/Card.java
+++ Task_1_1_2/src/main/java/ru/nsu/laptev/Card.java
@@ -1,5 +1,6 @@
 package ru.nsu.laptev;
 
+import java.util.ArrayList;
 /**
  *
  */
@@ -7,17 +8,19 @@ public class Card
 {
     protected String [] suits = {"Черви","Буби","Трефы","Пики"};
     protected String [] names = {"Двойка","Тройка","Четверка","Пятерка","Шестерка","Семерка","Восьмерка","Девятка","Десятка","Валет","Дама","Король","Туз"};
+    protected String [] short_names = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
 
-    public String[][] GenerateDeck()
+    public ArrayList<ArrayList<String>> GenerateDeck()
     {
-        String [][] deck = new String[52][2];
-        int indexSuit = 0;
-        int indexOfName = 0;
+        ArrayList<ArrayList <String>> deck = new ArrayList<>();
         for (int i = 0;i < 52;i++)
         {
-                deck[i][0] = suits[i / 13];
-                deck[i][1] = names [i % 13];
+            ArrayList<String> card = new ArrayList<>();
+            card.add(suits[i / 13]);
+            card.add(short_names[i % 13]);
+            deck.add(card);
         }
+
         return deck;
     }
  }
--- Task_1_1_2/src/main/java/ru/nsu/laptev/Main.java
+++ Task_1_1_2/src/main/java/ru/nsu/laptev/Main.java
@@ -1,18 +1,18 @@
 package ru.nsu.laptev;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Card card = new Card();
-        String[][] deck = new String[52][2];
+        ArrayList<ArrayList <String>> deck = new ArrayList<>();
         deck = card.GenerateDeck();
         for (int i = 0;i < 52;i++)
         {
-            System.out.println("Масть: " + deck[i][0] + " Data: " + deck[i][1]);
+            System.out.println("Масть: " + deck.get(i).get(0) + " Data: " + deck.get(i).get(1));
         }
-        int n = 0;
-        n += 1;
     }
 }
 
