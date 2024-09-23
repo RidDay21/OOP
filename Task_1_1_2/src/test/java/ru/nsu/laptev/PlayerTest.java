fea7cdad0771c19eed2e330adb2680226752f40d
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-09-23 20:10:13 +0700

    dl

--- Task_1_1_2/src/test/java/ru/nsu/laptev/PlayerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/laptev/PlayerTest.java
@@ -13,8 +13,5 @@ class PlayerTest {
         Player pl = new Player();
         assertEquals(pl.victories,0);
         assertEquals(pl.bust, 0);
-
-        ArrayList<ArrayList<String>> tmp = new ArrayList<>();
-        assertEquals(pl.hand.get(0).get(0), tmp.get(0).get(0));
     }
 }
