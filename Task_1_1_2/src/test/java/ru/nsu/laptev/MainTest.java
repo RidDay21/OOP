35df735578b9cba523740265da8d0f0b65a7adfd
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-09-24 21:09:11 +0700

    ustal

--- Task_1_1_2/src/test/java/ru/nsu/laptev/MainTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/laptev/MainTest.java
@@ -2,8 +2,12 @@ package ru.nsu.laptev;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MainTest {
 @Test
     void sampleTest() {
+            int tmp = 5;
+            assertEquals(tmp, 5);
     }
 }
