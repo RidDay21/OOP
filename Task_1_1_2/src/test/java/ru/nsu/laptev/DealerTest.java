afa135b1bef6f42c20c7de0ad2687eb879a21b4b
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-09-25 19:25:07 +0700

    DealerTest

--- Task_1_1_2/src/test/java/ru/nsu/laptev/DealerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/laptev/DealerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 
 class DealerTest {
-    Dealer dl = new Dealer();
+    Dealer dlr = new Dealer();
     Player pl = new Player();
     Deck deck = new Deck();
     Card card = new Card();
@@ -15,35 +15,41 @@ class DealerTest {
 
     @Test
     void sampleTest() {
-        assertEquals(dl.victories, 0);
-        assertEquals(dl.bust, 0);
+        assertEquals(dlr.victories, 0);
+        assertEquals(dlr.bust, 0);
         deck.deck = deck.generate_deck();
         assertEquals(deck.deck.size(), 52);
-        assertEquals(dl.hand.size(), 0);
-        dl.deal(deck, dl.hand);
-        assertEquals(dl.hand.size(), 1);
-        dl.deal(deck, dl.hand);
-        assertEquals(dl.hand.size(), 2);
-        dl.deal(deck, pl.hand);
-        dl.deal(deck, pl.hand);
+        assertEquals(dlr.hand.size(), 0);
+        dlr.deal(deck, dlr.hand);
+        assertEquals(dlr.hand.size(), 1);
+        dlr.deal(deck, dlr.hand);
+        assertEquals(dlr.hand.size(), 2);
+        dlr.deal(deck, pl.hand);
+        dlr.deal(deck, pl.hand);
         assertEquals(deck.deck.size(), 52 - 4);
         int size = deck.deck.size();
-        dl.shuffle(deck);
+        dlr.shuffle(deck);
         assertEquals(deck.deck.size(), size);
-        assertEquals(dl.is_ace("Туз"), 1);
-        assertEquals(dl.is_ace("Двойка"), 0);
-        dl.score = 21;
-        assertEquals(dl.score, 21);
-        dl.new_one();
-        assertEquals(dl.score, 0);
+        assertEquals(dlr.is_ace("Туз"), 1);
+        assertEquals(dlr.is_ace("Двойка"), 0);
+        dlr.score = 21;
+        assertEquals(dlr.score, 21);
+        dlr.new_one();
+        assertEquals(dlr.score, 0);
 
-        dl.deal(deck, dl.hand);
-        dl.deal(deck, dl.hand);
+        dlr.deal(deck, dlr.hand);
+        dlr.deal(deck, dlr.hand);
 
-        dl.firstTurn(values);
+        dlr.firstTurn(values);
 
-        dl.turn(deck, values, pl);
+        dlr.turn(deck, values, pl);
 
+        Game.result(pl, dlr, 3);
+        assertEquals(dlr.victories,0);
+        Game.result(pl, dlr, 1);
+        assertEquals(dlr.victories,0);
+        Game.result(pl, dlr, 0);
+        assertEquals(dlr.victories,1);
 
     }
 }
