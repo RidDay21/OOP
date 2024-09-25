43a8c3865c33da401abb9ff95ffc2a5bf77c8369
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-09-25 19:06:31 +0700

    cs1

---
+++ Task_1_1_2/src/test/java/ru/nsu/laptev/GameTest.java
@@ -0,0 +1,23 @@
+package ru.nsu.laptev;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+    @Test
+    void sampleTest()
+    {
+        Player pl = new Player();
+        String input;
+        InputStream inContent;
+        input = "0\n";
+        inContent = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inContent);
+        Game.game(1);
+    }
+
+}
