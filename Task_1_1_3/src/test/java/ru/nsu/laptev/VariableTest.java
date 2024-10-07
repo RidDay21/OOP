5b4edf95abbc24ef4af44fe9fbfa3640078e8e1a
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-10-08 00:39:34 +0700

    varTest

---
+++ Task_1_1_3/src/test/java/ru/nsu/laptev/VariableTest.java
@@ -0,0 +1,18 @@
+package ru.nsu.laptev;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VariableTest {
+    @Test
+    void sampleTest()
+    {
+        Variable var =  new Variable("x");
+        assertEquals(10, var.eval(" x = 10"));
+        assertEquals(0, var.eval(" y = 10"));
+        assertEquals(100, var.eval(" x = 10; x = 100"));
+        assertEquals(1, var.derivative("x").eval(""));
+        var.print();
+    }
+}
