38bca3056d2c908c06948fdfb1da192f0d6b90ff
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-10-08 00:58:48 +0700

    fix_print_sub

--- Task_1_1_3/src/test/java/ru/nsu/laptev/VariableTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/laptev/VariableTest.java
@@ -13,7 +13,8 @@ class VariableTest {
         assertEquals(0, var.eval(" y = 10"));
         assertEquals(100, var.eval(" x = 10; x = 100"));
         assertEquals(1, var.derivative("x").eval(""));
-        assertEquals(2, var.derivative("2x").eval(""));
+        var.print();
+        assertEquals(0, var.derivative("y").eval(""));
         var.print();
     }
 }
