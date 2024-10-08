f8ce7bd1e136abfdd4a8e3f74beba705b370c942
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-10-08 10:16:55 +0700

    ParserTests_and_cs

---
+++ Task_1_1_3/src/test/java/ru/nsu/laptev/ParserTest.java
@@ -0,0 +1,15 @@
+package ru.nsu.laptev;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ParserTest {
+    @Test
+    void sampleTest() {
+        int e = Parser.Parsing("x", "x = 10; x = 123; y = 0");
+        assertEquals(123, e);
+        e = Parser.Parsing("y", "");
+        assertEquals(0, e);
+    }
+}
