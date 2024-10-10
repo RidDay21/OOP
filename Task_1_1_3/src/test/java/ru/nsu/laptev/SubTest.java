58f55925447bc03dc70f3cf67448d89ae8df1db1
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-10-10 14:53:58 +0700

    test

--- Task_1_1_3/src/test/java/ru/nsu/laptev/SubTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/laptev/SubTest.java
@@ -26,5 +26,10 @@ class SubTest {
         assertEquals(84, sub.eval("x = 90"));
         sub = new Sub(new Variable("x"), new Number(5.056));
         Double res = sub.eval("x=      6.256");
+        int decimalPlaces = 2; //желаемое кол-во знаков после запятой
+        double numb = Math.round(res * Math.pow(10, decimalPlaces))/ Math.pow(10, decimalPlaces);
+        assertEquals(1.2, numb);
+        
+
     }
 }
