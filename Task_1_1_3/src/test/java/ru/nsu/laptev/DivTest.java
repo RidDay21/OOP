d5fd2e09cfa47c9ff4663268a8dabe2be917cf82
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-10-10 12:03:53 +0700

    split_DivTest_into_two_tests

--- Task_1_1_3/src/test/java/ru/nsu/laptev/DivTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/laptev/DivTest.java
@@ -8,20 +8,23 @@ import org.junit.jupiter.api.Test;
  * Class for testing class Div.
  */
 class DivTest {
+    Div div = new Div(new Number(4), new Number(2));
+
     @Test
-    void sampleTest() {
-        Div div = new Div(new Number(4), new Number(2));
+    void divTest() {
         div.print();
         assertEquals(2.0, div.eval(""));
         Expression e = div.derivative("x");
         div = new Div(new Number(5), new Number(0));
+    }
+
+    @Test
+    void divByZeroTest() {
         try {
             div.eval(" ");
         } catch (ArithmeticException exception) {
             System.out.println("Sheeeesh,bro, you tried to divide by zero...Not cool");
         }
-
-        e.print();
     }
 
 }
