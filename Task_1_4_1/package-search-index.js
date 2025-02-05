22630e94669da1c46c762b6095277f2c0ca5662e
RidDay21 <RidDay21@users.noreply.github.com>
2025-02-05 17:08:28 +0000

    Deploying to gh-pages from @ RidDay21/OOP@2641f1c2d3e9f57f6d2999cb1bbe95b34b24a93c 🚀

--- Task_1_4_1/package-search-index.js
+++ Task_1_4_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.laptev"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.laptev"}];updateSearchResults();
--- Task_1_4_1/type-search-index.js
+++ Task_1_4_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.laptev","l":"ExamSession"},{"p":"ru.nsu.laptev","l":"Grade"},{"p":"ru.nsu.laptev","l":"GradeValue"},{"p":"ru.nsu.laptev","l":"Student"},{"p":"ru.nsu.laptev","l":"StudentGradeBook"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.laptev","l":"ExamSession"},{"p":"ru.nsu.laptev","l":"Grade"},{"p":"ru.nsu.laptev","l":"GradeValue"},{"p":"ru.nsu.laptev","l":"Student"},{"p":"ru.nsu.laptev","l":"StudentGradeBook"}];updateSearchResults();
